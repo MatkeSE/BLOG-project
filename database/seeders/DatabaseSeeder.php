f530646795eb12182ffe26fa789ee4d69204b61d
unknown <matijailicmatke@gmail.com>
2024-05-09 10:13:31 +0200

    Migracije za tabele odradjene, za kategorije i tagove koji u bazi sadrze jedan string i status(aktivne 1/neaktivne 0) umesto cruda na jednom stringu i integeru, te 2 tabele sam napunio preko seed-era uz pomoc factory klase (php artisan make:factory name), zatim preko DatabaseSeeder API-a su tabele u bazi popunjene funkcijom php artisan db:seed. Next task - CRUD nad blogovima sa foreignID-evima kategorije i taga

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Category;
+use App\Models\Tag;
 use App\Models\User;
+use GuzzleHttp\Promise\Create;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -15,9 +18,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        // User::factory()->create([
+        //     'name' => 'Test User',
+        //     'email' => 'test@example.com',
+        // ]);
+
+        Tag::factory(7)->create();
+        Category::factory(6)->create();
+
+
     }
 }
